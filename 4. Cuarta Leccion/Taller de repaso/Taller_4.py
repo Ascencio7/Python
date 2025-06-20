822870bb572953be3e3f3bf12a62ad183c7a0a12
Vladimir Ascencio <vladimirramos405@gmail.com>
2025-06-20 11:49:48 -0600

    Taller 4 completo
    Ejercicios de bucle terminados

--- 4. Cuarta Leccion/Taller de repaso/Taller_4.py
+++ 4. Cuarta Leccion/Taller de repaso/Taller_4.py
@@ -1,7 +1,5 @@
 # 1. Del conjunto de datos [20, 21, 30, 41, 50, 51, 201] mostrar en pantalla los que no terminen en 1.
-
 print("\n1. Del conjunto de datos [20, 21, 30, 41, 50, 51, 201] mostrar en pantalla los que no terminen en 1.")
-
 print("\n")
 
 for i in [10, 11, 20, 21, 30, 31, 40, 41, 50, 51, 60, 61, 70, 71, 80, 81, 90, 91, 100, 101, 200, 201]:
@@ -9,11 +7,9 @@ for i in [10, 11, 20, 21, 30, 31, 40, 41, 50, 51, 60, 61, 70, 71, 80, 81, 90, 91
         print(i)
 print("\nFin")
 
-print("\n")
 
 # 2. Mostrar 10 veces la palabra "Programacion".
 print("\n2. Mostrar 10 veces la palabra Programacion")
-
 print("\n")
 
 for i in range(10):
@@ -46,4 +42,40 @@ print("\n")
 for i in range(len(dias)):
     print(letras[i], " = ", dias[i])
 
+print("\nFin")
+
+
+# 5. Del conjunto de datos ["Omar", 54, 2, 2, "Luis", 45, 3, "Juan", 39, 1] mostrar en pantalla los datos organizados en 3 columnas que tengan
+# por titulo Nombre, Edad e Hijos
+
+print("\n5. Del conjunto de datos [Omar, 54, 2, 2, Luis, 45, 3, Juan, 39, 1] mostrar en pantalla los datos organizados en 3 columnas que tengan por titulo Nombre, Edad e Hijos")
+datos = ["Omar", 54, 2, "Luis", 45, 3, "Juan", 39,1]
+print("\n")
+print("{:<10} {:<5} {:<5}".format("Nombre", "Edad", "Hijos"))
+
+for i in range(0, len(datos), 3):
+    nombre = datos[i]
+    edad = datos[i + 1]
+    hijos = datos[i + 2]
+    print("{:<10} {:<5} {:<5}".format(nombre, edad, hijos))
+print("\nFin")
+
+
+# 6. Leer una cadena de caracteres y mostrarla verticalmente.
+print("\n6. Mostrar la cadena de texto verticalmente")
+cadena = str(input("\nIngresa una cadena de texto: "))
+print("\n")
+for letra in cadena:
+    print(letra)
+print("\nFin")
+
+
+# 7. Leer una cadena y determinar cuantas veces aparece la letra A.
+print("\n7. Leer una cadena y determinar cuantas veces aparece la letra A")
+cadena = str(input("\nIngresa una cadena de texto: "))
+contador = 0
+for letra in cadena:
+    if letra == 'A': # mejor como comparador porque si se usa upper o lower cuenta las minusculas tambien
+        contador += 1
+print(f"La letra 'A' aparece {contador} veces en la cadena.")
 print("\nFin")
